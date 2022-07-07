216aa1fc5fd6682eb02bb7753aa325d0e077881a
fufako <patryko.mikiciuk@gmail.com>
2022-07-07 17:45:29 +0200

    Create constructor function for Book object

--- script.js
+++ script.js
@@ -1,9 +1,17 @@
 let myLibrary = [];
-
-function Book() {
-  // the constructor...
+const addBook = document.getElementById('add')
+const popup = document.getElementById("popup")
+function Book(author, title, pages) {
+    this.author = author
+    this.title = title
+    this.pages = pages
+    this.read = false
 }
 
 function addBookToLibrary() {
   // do stuff here
+}
+
+function openPopup (){
+    popup.style.removeProperty('visible')
 }
